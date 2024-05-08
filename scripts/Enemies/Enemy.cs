5c1f5ab74a9dc0967b9ef6fc9447481ee3dbb9f5
Terry Burns-Dyson <terrybd@gmail.com>
2024-05-07 22:37:52 -0400

    Explode before disappear on enemy
    Added a few more enemies, added explosion on hit of enemy

--- scripts/Enemies/Enemy.cs
+++ scripts/Enemies/Enemy.cs
@@ -4,7 +4,15 @@ using Godot;
 
 public partial class Enemy : Area2D, IScoreable
 {
+    [Export] private Timer _endingTimer;
     public event Action<IScoreable> CollisionOccured;
+    private Sprite2D _explosion;
+
+    public override void _Ready()
+    {
+        _endingTimer.Timeout += QueueFree;
+        _explosion = GetNode<Sprite2D>("Explosion");
+    }
 
     private void OnUnitHit(Area2D collidedWith)
     {
@@ -17,7 +25,16 @@ public partial class Enemy : Area2D, IScoreable
 
         CollisionOccured?.Invoke(this);
 
-        QueueFree();
+        _endingTimer.Start();
+        _explosion.Visible = true;
+    }
+
+    private void __ExitTree()
+    {
+        _endingTimer.Timeout -= QueueFree;
+        _endingTimer.QueueFree();
+
+        _endingTimer = null;
     }
 
     [Export] public int Score { get; set; }
